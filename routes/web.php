62e0ede9ac5c2e4f707695ef3a308dab5b123b45
Leoramirezb <leopoldo_ramirezbenitez@iescarlesvallbona.cat>
2022-12-04 15:46:39 +0100

    c

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,17 @@ Route::get('llibre/list', [LlibreController::class, 'list'])->name('llibre_list'
 
 //Route::get('/llibre/edit/{id}', [LlibreController::class, 'edit'])->name('llibre_edit');
 
-Route::match(['get', 'post'], '/llibre/edit', [LlibreController::class, 'edit'])->name('llibre_edit');
+Route::match(['get', 'post'], '/llibre/edit/{id}', [LlibreController::class, 'edit'])->name('llibre_edit');
 
 Route::match(['get', 'post'], '/llibre/new', [LlibreController::class, 'new'])->name('llibre_new');
 
 Route::get('/llibre/delete/{id}', [LlibreController::class, 'delete'])->name('llibre_delete');
 
-//Route::get('/autor/list', [AutorController::class, 'list'])->name('autor_list');
+Route::get('/autor/list', [AutorController::class, 'list'])->name('autor_list');
 
-Route::get('/autor/edit/{id}', [AutorController::class, 'edit'])->name('autor_edit');
+//Route::get('/autor/edit/{id}', [AutorController::class, 'edit'])->name('autor_edit');
 
-Route::match(['get', 'post'], '/autor/edit', [AutorController::class, 'edit'])->name('autor_edit');
+Route::match(['get', 'post'], '/autor/edit({id}', [AutorController::class, 'edit'])->name('autor_edit');
 
 Route::match(['get', 'post'], '/autor/new', [AutorController::class, 'new'])->name('autor_new');
 
